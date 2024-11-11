82a7bf5525ade0f06e6320ae4e9172b8a35573c9
Himanshu Saini <himanshusaini0345@outlook.com>
2024-11-11 11:37:52 +0530

    remove ui user search

--- ChatApplication/Hubs/ChatHub.cs
+++ ChatApplication/Hubs/ChatHub.cs
@@ -1,56 +1,54 @@
 ﻿using ChatApplication.Models;
 using Microsoft.AspNetCore.SignalR;
 
-namespace ChatApplication.Hubs
+namespace ChatApplication.Hubs;
+
+public class ChatHub : Hub
 {
-    public class ChatHub : Hub
+    private readonly IDictionary<string, UserRoomConnection> _connection;
+
+    public ChatHub(IDictionary<string, UserRoomConnection> connection)
     {
-        private readonly IDictionary<string,UserRoomConnection> _connections;
+        _connection = connection;
+    }
 
-        public ChatHub(IDictionary<string, UserRoomConnection> connections)
+    public async Task JoinRoom(UserRoomConnection userConnection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room!);
+        _connection[Context.ConnectionId] = userConnection;
+        await Clients.Group(userConnection.Room!)
+            .SendAsync("ReceiveMessage", "System", $"{userConnection.User} has Joined the Group", DateTime.Now);
+        await SendConnectedUser(userConnection.Room!);
+    }
+
+    public async Task SendMessage(string message)
+    {
+        if (_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection userRoomConnection))
         {
-            _connections = connections;
+            await Clients.Group(userRoomConnection.Room!)
+                .SendAsync("ReceiveMessage", userRoomConnection.User, message, DateTime.Now);
+        }
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exp)
+    {
+        if (!_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection roomConnection))
+        {
+            return base.OnDisconnectedAsync(exp);
         }
 
-        public async Task JoinRoom(UserRoomConnection userRoomConnection)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, userRoomConnection.Room);
-            _connections[Context.ConnectionId] = userRoomConnection;
-            await Clients.Group(userRoomConnection.Room)
-                         .SendAsync("RecieveMessage", "Lets Program Bot", $"{userRoomConnection.User} has joined the room.");
+        _connection.Remove(Context.ConnectionId);
+        Clients.Group(roomConnection.Room!)
+            .SendAsync("ReceiveMessage", "Lets Program bot", $"{roomConnection.User} has Left the Group", DateTime.Now);
+        SendConnectedUser(roomConnection.Room!);
+        return base.OnDisconnectedAsync(exp);
+    }
 
-        }
-
-        public async Task Sendmessage(string message)
-        {
-            if(_connections.TryGetValue(Context.ConnectionId, out UserRoomConnection? userRoomConnection))
-            {
-                await Clients.Group(userRoomConnection.Room)
-                             .SendAsync("RecieveMessage", userRoomConnection.User, message, DateTime.Now);
-            }
-        }
-
-        public async Task SendConnectedUser(string room)
-        {
-            var users = _connections.Values
-                                    .Where(urc => urc.Room == room)
-                                    .Select(x => x.User);
-
-            await Clients.Group(room)
-                          .SendAsync("ConnectedUser", users);
-
-        }
-
-        public override async Task OnDisconnectedAsync(Exception? exception)
-        {
-            if(!_connections.TryGetValue(Context.ConnectionId,out UserRoomConnection? userRoomConnection))
-            {
-                await base.OnDisconnectedAsync(exception);
-            }
-
-            await Clients.Group(userRoomConnection.Room)
-                         .SendAsync("RecieveMessage", "Lets Program Bot", $"{userRoomConnection.User} has left the group");
-            await SendConnectedUser(userRoomConnection.Room);
-        }
+    public Task SendConnectedUser(string room)
+    {
+        var users = _connection.Values
+            .Where(u => u.Room == room)
+            .Select(s => s.User);
+        return Clients.Group(room).SendAsync("ConnectedUser", users);
     }
 }
